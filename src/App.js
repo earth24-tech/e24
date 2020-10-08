83f9b2cf9bed5529a83c0ec7d660d3937ff2761f
vipul <vipul.khandelwal23@gmail.com>
2020-10-08 11:59:16 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,32 @@
+import React from "react";
+import "./App.css";
+import Main from "./components/Main/Main";
+import JoinUs from "./components/JoinUs/JoinUs";
+import Features from "./components/Features/Features";
+import BigOffer from "./components/BigOffer/BigOffer";
+import Pricing from "./components/Pricing/Pricing";
+import Gallery from "./components/Gallery/Gallery";
+import Trainers from "./components/Trainers/Trainers";
+import BannerOffer from "./components/BannerOffer/BannerOffer";
+import Footer from "./components/Footer/Footer";
+
+const App = () => {
+  return (
+    <div className="App">
+ <h1>We will be live soon...</h1>
+ <h2>Welcome to earth24 technology!</h2>
+ <p>https://www.e24.tech</p>
+      {/* <Main />
+      <JoinUs />
+      <Features />
+      <BigOffer />
+      <Pricing />
+      <Gallery />
+      <Trainers />
+      <BannerOffer />
+      <Footer /> */}
+    </div>
+  );
+};
+
+export default App;
